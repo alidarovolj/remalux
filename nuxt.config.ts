c9b5381b698932e32985f4ddeb5f2c08151a8a5d
alidarovolj <alidarovolzhas@gmail.com>
2024-08-29 03:07:35 +0500

    favicon fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@ export default defineNuxtConfig({
     app: {
         head: {
             link: [
-                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-                { rel: 'manifest', href: '/site.webmanifest' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+                { rel: 'manifest', href: '/favicon/site.webmanifest' },
             ],
             meta: [
                 { name: 'msapplication-TileColor', content: '#da532c' },
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
             ],
         },
     },
+
 
     devtools: {enabled: true},
     css: [
