ea5e9615af16b87a747ac72ccfae24b111c0b6ac
alidarovolj <alidarovolzhas@gmail.com>
2024-09-25 23:01:38 +0500

    done with empty reviews

--- pages/store/[id].vue
+++ pages/store/[id].vue
@@ -679,9 +679,12 @@ useHead({
             Общая оценка
           </p>
           <div class="flex gap-3 mb-4">
-            <p class="text-6xl">{{ detailProduct.rating.rating }}</p>
+            <p class="text-6xl" v-if="detailProduct.rating">{{ detailProduct.rating.rating }}</p>
+            <p class="text-6xl" v-else>0</p>
             <div class="flex flex-col justify-between">
-              <div class="flex gap-1">
+              <div
+                  v-if="detailProduct.rating"
+                  class="flex gap-1">
                 <svg
                     v-for="(item, index) of Math.round(detailProduct.rating.rating)"
                     :key="index"
@@ -697,6 +700,23 @@ useHead({
                 </svg>
                 <svg
                     v-for="(item, index) of Math.round(5 - detailProduct.rating.rating)"
+                    :key="index"
+                    class="size-6 text-[#D6D6D6]"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      clip-rule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                      fill-rule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div
+                  v-else
+                  class="flex gap-1">
+                <svg
+                    v-for="(item, index) of 5"
                     :key="index"
                     class="size-6 text-[#D6D6D6]"
                     fill="currentColor"
